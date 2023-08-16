7a329ac1e1998d6b60ce379483e2b032d8c79e53
muza <muzallyefah@gmail.com>
2023-08-17 01:33:24 +0800

    feat: add authentication module page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullComponent } from './layout/full/full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        redirectTo: '/auth/login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'auth',
+        component: FullComponent,
+        loadChildren: () =>
+          import('./pages/auth/auth.module').then(
+            (m) => m.AuthModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TablerIconsModule } from 'angular-tabler-icons';
+import * as TablerIcons from 'angular-tabler-icons/icons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material.module';
+import { FullComponent } from './layout/full/full.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    FullComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -20,8 +26,11 @@ import { FormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     FormsModule, 
     MatFormFieldModule, 
-    MatInputModule
+    MatInputModule,
+    MaterialModule,
+    TablerIconsModule.pick(TablerIcons),
   ],
+  exports: [TablerIconsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
