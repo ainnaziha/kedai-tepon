00240fef5150f50627b73c5c8bfce9a7911b8e36
ainnaziha <100841335+ainnaziha@users.noreply.github.com>
2023-08-16 23:35:22 +0800

    dependency: material UI (#1)
    * materia ui
    
    * app bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    MatFormFieldModule, 
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
