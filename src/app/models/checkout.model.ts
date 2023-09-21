aa7e293a58b8127692439f37d6e37ea812e0f6e5
muzalee <85381407+muzalee@users.noreply.github.com>
2023-09-22 00:04:51 +0800

    feat: add checkout model

---
+++ src/app/models/checkout.model.ts
@@ -0,0 +1,21 @@
+export class Checkout {
+  id: number;
+  orderNo: string;
+  invoiceNo: string;
+  total: string;
+  name: string;
+  email: string;
+  street: string;
+  town: string;
+
+  constructor(data: any) {
+    this.id = data.id;
+    this.orderNo = data.orderNo;
+    this.invoiceNo = data.invoiceNo ?? '';
+    this.total = data.total ?? '';
+    this.name = data.name ?? '';
+    this.email = data.email ?? '';
+    this.town = data.town ?? '';
+    this.street = data.street ?? '';
+  }
+}
